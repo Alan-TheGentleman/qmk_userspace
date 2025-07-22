73b62824424d6f380c9745b6dad22a2002a47060
Alan Buscaglia <alanbuscaglia@Alans-MacBook-Pro.local>
2025-07-23 00:59:50 +0200

    fix: test 135

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/config.h
@@ -27,7 +27,7 @@
 #endif // __arm__
 
 /* Tapping configuration */
-#define TAPPING_TERM 150
+#define TAPPING_TERM 135
 #define PERMISSIVE_HOLD
 // #define IGNORE_MOD_TAP_INTERRUPT
 // #define TAPPING_FORCE_HOLD
