0de034bc5359599b316d8e70649fc7ed76da94d7
Alan Buscaglia <abuscagl@ext.unicaja.es>
2025-07-25 15:37:00 +0200

    feat: better navigation to imitate nvim

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
@@ -60,10 +60,10 @@ static uint16_t auto_pointer_layer_timer = 0;
 // clang-format off
 /** \brief QWERTY layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                     \
-       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
-       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, \
-       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
-                      ESC_MED, SPC_COD, TAB_NUM, KC_ENT, BSP_NAV
+KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
+KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, \
+KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
+ESC_MED, SPC_COD, TAB_NUM, KC_ENT, BSP_NAV
 
 /** Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
@@ -90,10 +90,10 @@ static uint16_t auto_pointer_layer_timer = 0;
  * from the base layer to enable auto-repeat.
  */
 #define LAYOUT_LAYER_NUMERAL                                                                 \
-    XXXXXXX, XXXXXXX, KC_F7,KC_F8, KC_F9, XXXXXXX,   KC_7,   KC_8,   KC_9,  XXXXXXX, \
-    KC_LSFT, KC_LALT, KC_F4,KC_F5, KC_F6, XXXXXXX,   KC_4,   KC_5,   KC_6,  KC_0, \
-    KC_LGUI, KC_LCTL, KC_F1,KC_F2, KC_F3, XXXXXXX,   KC_1,   KC_2,   KC_3,  XXXXXXX, \
-                      XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX
+XXXXXXX, XXXXXXX, KC_F7,KC_F8, KC_F9, XXXXXXX,   KC_7,   KC_8,   KC_9,  XXXXXXX, \
+KC_LSFT, KC_LALT, KC_F4,KC_F5, KC_F6, XXXXXXX,   KC_4,   KC_5,   KC_6,  KC_0, \
+KC_LGUI, KC_LCTL, KC_F1,KC_F2, KC_F3, XXXXXXX,   KC_1,   KC_2,   KC_3,  XXXXXXX, \
+XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX
 
 
 /**
@@ -105,10 +105,10 @@ static uint16_t auto_pointer_layer_timer = 0;
  * from the base layer to enable auto-repeat.
  */
 #define LAYOUT_LAYER_CODING                                                                 \
-    XXXXXXX, XXXXXXX, XXXXXXX,S(KC_4), XXXXXXX, S(KC_6),S(KC_9), S(KC_0),   KC_MINS,  XXXXXXX, \
-    KC_LSFT, S(KC_2), S(KC_3),S(KC_5), XXXXXXX, S(KC_7),  S(KC_LBRC),   S(KC_RBRC),   KC_EQL, KC_QUOT, \
-     KC_GRV, XXXXXXX, XXXXXXX,S(KC_SLSH), XXXXXXX, S(KC_8),   KC_LBRC,   KC_RBRC,   S(KC_1),  KC_BSLS, \
-                       KC_LGUI, _______, KC_LALT, KC_ENT, KC_LCTL
+XXXXXXX, XXXXXXX, XXXXXXX,S(KC_4), XXXXXXX, S(KC_6),S(KC_9), S(KC_0),   KC_MINS,  XXXXXXX, \
+KC_LSFT, S(KC_2), S(KC_3),S(KC_5), XXXXXXX, S(KC_7),  S(KC_LBRC),   S(KC_RBRC),   KC_EQL, KC_QUOT, \
+KC_GRV, XXXXXXX, XXXXXXX,S(KC_SLSH), XXXXXXX, S(KC_8),   KC_LBRC,   KC_RBRC,   S(KC_1),  KC_BSLS, \
+KC_LGUI, _______, KC_LALT, KC_ENT, KC_LCTL
 
 /**
  * \brief Media layer.
@@ -117,17 +117,17 @@ static uint16_t auto_pointer_layer_timer = 0;
  * symmetrical to accomodate the left- and right-hand trackball.
  */
 #define LAYOUT_LAYER_MEDIA                                                                    \
-    XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX, XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX, \
-    KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, \
-    DT_UP, DT_DOWN, DT_PRNT,  EE_CLR, QK_BOOT, QK_BOOT,  EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, \
-                      _______, KC_MPLY, KC_MSTP, KC_MSTP, KC_MPLY
+XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX, XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX, \
+KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, \
+DT_UP, DT_DOWN, DT_PRNT,  EE_CLR, QK_BOOT, QK_BOOT,  EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, \
+_______, KC_MPLY, KC_MSTP, KC_MSTP, KC_MPLY
 
 /** \brief Mouse emulation and pointer functions. */
 #define LAYOUT_LAYER_POINTER                                                                  \
-    QK_BOOT,  EE_CLR, XXXXXXX, DPI_MOD, S_D_MOD, S_D_MOD, DPI_MOD, XXXXXXX,  EE_CLR, QK_BOOT, \
-    ______________HOME_ROW_GACS_L______________, ______________HOME_ROW_GACS_R______________, \
-    _______, DRGSCRL, SNIPING, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SNIPING, DRGSCRL, _______, \
-                      KC_BTN2, KC_BTN1, KC_BTN3, KC_BTN3, KC_BTN1
+QK_BOOT,  EE_CLR, XXXXXXX, DPI_MOD, S_D_MOD, S_D_MOD, DPI_MOD, XXXXXXX,  EE_CLR, QK_BOOT, \
+______________HOME_ROW_GACS_L______________, ______________HOME_ROW_GACS_R______________, \
+_______, DRGSCRL, SNIPING, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SNIPING, DRGSCRL, _______, \
+KC_BTN2, KC_BTN1, KC_BTN3, KC_BTN3, KC_BTN1
 
 /**
  * \brief Navigation layer.
@@ -138,10 +138,10 @@ static uint16_t auto_pointer_layer_timer = 0;
  * base layer to avoid having to layer change mid edit and to enable auto-repeat.
  */
 #define LAYOUT_LAYER_NAVIGATION                                                               \
-    _______________DEAD_HALF_ROW_______________, _______________DEAD_HALF_ROW_______________, \
-    ______________HOME_ROW_GACS_L______________, KC_CAPS, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, \
-    _______________DEAD_HALF_ROW_______________,  KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, \
-                      XXXXXXX, _______, XXXXXXX,  KC_ENT, KC_BSPC
+_______________DEAD_HALF_ROW_______________, _______________DEAD_HALF_ROW_______________, \
+______________HOME_ROW_GACS_L______________,  KC_LEFe, KC_DOWN,   KC_UP, KC_RGHT, KC_CAPS \
+_______________DEAD_HALF_ROW_______________,  KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, \
+XXXXXXX, _______, XXXXXXX,  KC_ENT, KC_BSPC
 
 
 /**
@@ -155,14 +155,14 @@ static uint16_t auto_pointer_layer_timer = 0;
  *     HOME_ROW_MOD_GACS(LAYER_ALPHAS_QWERTY)
  */
 #define _HOME_ROW_MOD_GACS(                                            \
-    L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
-    L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
-    ...)                                                               \
-             L00,         L01,         L02,         L03,         L04,  \
-             R05,         R06,         R07,         R08,         R09,  \
-      LGUI_T(L10), LALT_T(L11), LCTL_T(L12), LSFT_T(L13),        L14,  \
-             R15,  RSFT_T(R16), RCTL_T(R17), LALT_T(R18), RGUI_T(R19), \
-      __VA_ARGS__
+L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
+L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
+...)                                                               \
+L00,         L01,         L02,         L03,         L04,  \
+R05,         R06,         R07,         R08,         R09,  \
+LGUI_T(L10), LALT_T(L11), LCTL_T(L12), LSFT_T(L13),        L14,  \
+R15,  RSFT_T(R16), RCTL_T(R17), LALT_T(R18), RGUI_T(R19), \
+__VA_ARGS__
 #define HOME_ROW_MOD_GACS(...) _HOME_ROW_MOD_GACS(__VA_ARGS__)
 
 /**
@@ -176,30 +176,30 @@ static uint16_t auto_pointer_layer_timer = 0;
  *     POINTER_MOD(LAYER_ALPHAS_QWERTY)
  */
 #define _POINTER_MOD(                                                  \
-    L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
-    L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
-    L20, L21, L22, L23, L24, R25, R26, R27, R28, R29,                  \
-    ...)                                                               \
-             L00,         L01,         L02,         L03,         L04,  \
-             R05,         R06,         R07,         R08,         R09,  \
-             L10,         L11,         L12,         L13,         L14,  \
-             R15,         R16,         R17,         R18,         R19,  \
-      _L_PTR(L20),        L21,         L22,         L23,         L24,  \
-             R25,         R26,         R27,         R28,  _L_PTR(R29), \
-      __VA_ARGS__
+L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
+L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
+L20, L21, L22, L23, L24, R25, R26, R27, R28, R29,                  \
+...)                                                               \
+L00,         L01,         L02,         L03,         L04,  \
+R05,         R06,         R07,         R08,         R09,  \
+L10,         L11,         L12,         L13,         L14,  \
+R15,         R16,         R17,         R18,         R19,  \
+_L_PTR(L20),        L21,         L22,         L23,         L24,  \
+R25,         R26,         R27,         R28,  _L_PTR(R29), \
+__VA_ARGS__
 #define POINTER_MOD(...) _POINTER_MOD(__VA_ARGS__)
 
 #define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LAYER_BASE] = LAYOUT_wrapper(
-    POINTER_MOD(HOME_ROW_MOD_GACS(LAYOUT_LAYER_BASE))
-  ),
-  [LAYER_NAVIGATION] = LAYOUT_wrapper(LAYOUT_LAYER_NAVIGATION),
-  [LAYER_MEDIA] = LAYOUT_wrapper(LAYOUT_LAYER_MEDIA),
-  [LAYER_NUMERAL] = LAYOUT_wrapper(LAYOUT_LAYER_NUMERAL),
-  [LAYER_POINTER] = LAYOUT_wrapper(LAYOUT_LAYER_POINTER),
-  [LAYER_CODING] = LAYOUT_wrapper(LAYOUT_LAYER_CODING),
+    [LAYER_BASE] = LAYOUT_wrapper(
+        POINTER_MOD(HOME_ROW_MOD_GACS(LAYOUT_LAYER_BASE))
+    ),
+    [LAYER_NAVIGATION] = LAYOUT_wrapper(LAYOUT_LAYER_NAVIGATION),
+    [LAYER_MEDIA] = LAYOUT_wrapper(LAYOUT_LAYER_MEDIA),
+    [LAYER_NUMERAL] = LAYOUT_wrapper(LAYOUT_LAYER_NUMERAL),
+    [LAYER_POINTER] = LAYOUT_wrapper(LAYOUT_LAYER_POINTER),
+    [LAYER_CODING] = LAYOUT_wrapper(LAYOUT_LAYER_CODING),
 };
 // clang-format on
 
